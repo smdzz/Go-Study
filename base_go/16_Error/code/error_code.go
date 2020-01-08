6df862a1c14bb71918663225324b9136e5bbd38e
mengd <sjzxjmd@163.com>
2020-01-08 21:37:27 +0800

    error

--- base_go/16_Error/code/error_code.go
+++ base_go/16_Error/code/error_code.go
@@ -51,18 +51,23 @@
 //	fmt.Println(addDevide(100, 200))
 //}
 
-
 // 详解 panic defer recover异常处理
 package main
 
 import (
 	"errors"
 	"fmt"
+	"log"
+	"time"
 )
 
 func main() {
 	testError()
 	fmt.Println("当上面函数的panic被recover()捕获后,我就可以继续执行了")
+	// test1 返回 第二个异常,第二个异常被捕获,recover()捕获最后一个异常
+	test1()
+	// throwsPanic(genErr)
+	throwsPanic(genErr)
 }
 
 func testError() {
@@ -78,4 +83,44 @@ func testError() {
 	// panic主动抛出异常
 	panic(errors.New("我这里出现错误,运行不下去了"))
 	//os.Exit(1)
+}
+
+func test1() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("捕获到的异常: %v", r)
+		}
+	}()
+
+	defer func() {
+		panic("第二个异常")
+	}()
+
+	defer func() {
+		panic("第三个异常")
+	}()
+
+	panic("第一个异常")
+}
+
+func genErr() {
+	fmt.Println("这里是正常执行的语句")
+	defer func() {
+		fmt.Println(time.Now(), "defer正常语句")
+		panic("第二个错误")
+	}()
+	panic("第一个错误")
+}
+
+// 虽然第一个错误早就发生了,但是在recover函数中看不到第一个错误被捕获,这是因为recover只会捕获最后一个错误,
+// 而且捕获的时机是在最后面,不影响第一个错误之后defer的继续执行
+func throwsPanic(f func()) (b bool) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(time.Now(), "捕获到的异常: ", r)
+			b = true
+		}
+	}()
+	f()
+	return
 }
