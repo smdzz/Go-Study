905d65e7029f0755c060e681796603f6a93ffee1
mengde <mengde@xiaoshouyi.com>
2020-07-28 00:12:06 +0800

    interface

--- base_go/15_Interface/code/interface_code.go
+++ base_go/15_Interface/code/interface_code.go
@@ -1,27 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-type DogClass interface {
-	changeName(dog Dog, name string)
-	changeColor(color string)
+type Usb interface {
+	// 声明两个没有实现的方法
+	Start()
+	Stop()
 }
 
-type Dog struct {
-	name string
-	age int
-	color string
+type Phone struct {
 }
 
-// 想要修改Dog的属性,必须要传入dog的指针结构体,才可以进行修改
-// (dog *Dog)声明属于哪个结构体, changeName为上面定义好的函数,  后面的string为返回值类型
-func (dog *Dog) changeName(name string) string {
-	dog.name = name
-	return dog.name
+func (p Phone) Start() {
+	fmt.Println("手机开始工作...")
 }
 
+func (p Phone) Stop() {
+	fmt.Println("手机停止工作...")
+}
+
+type Camera struct {
+}
+
+func (c Camera) Start() {
+	fmt.Println("相机开始工作...")
+}
+
+func (c Camera) Stop() {
+	fmt.Println("相机停止工作...")
+}
+
+type Computer struct {
+
+}
+
+// 接收一个Usb接口类型变量,只要实现了Usb接口 (所谓实现了Usb接口,就是指实现了Usb接口声明的所有方法)
+// 多态在python中通过方法来实现,在Go中通过接口来实现
+func (c Computer) Working(usb Usb) {
+	// 通过usb接口变量来调用start和stop方法
+	fmt.Println(reflect.TypeOf(usb))
+	usb.Start()
+	usb.Stop()
+}
+
+type AInterface interface {
+	Say()
+}
+
+type BInterface interface {
+	Hello()
+}
+// #############################################
+type Monster struct {
+
+}
+
+func (m Monster) Say() {
+	fmt.Println("monster say() ...")
+}
+
+func (m Monster) Hello() {
+	fmt.Println("monster hello() ...")
+}
+// #############################################
+
+// *****************************接口继承
+type CInterface interface {
+	AInterface
+	BInterface
+	test()
+}
+
+func (m Monster) test() {
+	fmt.Println("monster test() ...")
+}
+// *****************************
 func main() {
-	dog1 := new(Dog)
-	fmt.Println(dog1.changeName("xiogou"))
-	fmt.Println(dog1)
+	computer := Computer{}
+
+	phone := Phone{}
+	computer.Working(phone)
+
+	camera := Camera{}
+	computer.Working(camera)
+
+	// #############################################
+	// Monster实现了AInterface和BInterface
+	var monster Monster
+	var a1 AInterface = monster
+	var b1 BInterface = monster
+	a1.Say()
+	b1.Hello()
+	// #############################################
+
+	// *****************************
+	// Monster实现了CInterface中的所有方法
+	var c1 CInterface = monster
+	c1.Hello()
+	c1.Say()
+	c1.test()
+	// *****************************
 }
